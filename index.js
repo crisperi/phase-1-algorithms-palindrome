83012dddb7a498d9affae7fa0598ea33b64d2506
crisperi <phoenixedian@gmail.com>
2022-08-06 10:05:43 -0700

    finished the 1st part

--- index.js
+++ index.js
@@ -1,11 +1,41 @@
-function isPalindrome(word) {
+function isPalindrome(text) {
   // Write your algorithm here
+  //reverse the input string 
+  //if the reversed string is the same as the input 
+  // return true
+  //else
+  //return false
+
+//reverse the input string 
+const reverseWord = reverseString(word);
+
+//if the reversed string === input
+if (word === reverseWord){
+  return true;
+
+}else {
+  return false;
 }
+
+}
+
+
 
 /* 
   Add your pseudocode here
-*/
 
+*/
+function reverseString(word){
+  //TODO : implement string reversing functionality
+const wordArray = word.split("");
+
+const reversedWordArray = wordArray.reverse();
+
+const reversedWord = reversedWordArray.join("");
+
+
+  return reversedWord;
+}
 /*
   Add written explanation of your solution here
 */
@@ -13,6 +43,11 @@ function isPalindrome(word) {
 // You can run `node index.js` to view these console logs
 if (require.main === module) {
   // add your own custom tests in here
+console.log("Expecting : ih");
+console.log("=>",reverseString("hi"));
+
+console.log("");
+
   console.log("Expecting: true");
   console.log("=>", isPalindrome("racecar"));
 
